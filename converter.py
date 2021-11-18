390cfe85147a881d1bbf0b75440f374f009b432b
fanthomless <digonto.pvt.2021@gmail.com>
2021-11-18 14:24:18 +0530

    Changed pdf parser to pymupdf, much better text output now

--- converter.py
+++ converter.py
@@ -2,6 +2,9 @@ from io import StringIO
 import os
 from util import prepDir
 
+import PyPDF2
+import fitz
+
 from pdfminer.converter import TextConverter, XMLConverter, HTMLConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
@@ -13,33 +16,29 @@ pdf_directory = "./assets/papers/IJCNLP/pdf/"
 txt_directory = "./assets/papers/IJCNLP/txt/"
 xml_directory = "./assets/papers/IJCNLP/xml/"
 
-def convertPapers():
+def convertPapers() -> None:
     for _, pdf_file_name in enumerate(os.listdir(pdf_directory)):
         suffix = pdf_file_name[pdf_file_name.index("doc") + 3:pdf_file_name.index(".pdf")]
         pdf_file_path = os.path.join(pdf_directory, f"doc{suffix}.pdf")
 
         if os.path.isfile(pdf_file_path):
-            with open(pdf_file_path, "rb") as pdf_file:
-
+            with fitz.Document(pdf_file_path) as pdf_file:
                 with open(os.path.join(txt_directory, f"txt{suffix}.txt"), "w", encoding="utf-8") as txt_output_file:
-                    parser = PDFParser(pdf_file)
-                    doc = PDFDocument(parser)
-                    rsrcmgr = PDFResourceManager()
-                    codec = 'utf-8'
-                    device = TextConverter(rsrcmgr, txt_output_file, laparams=LAParams())
-                    interpreter = PDFPageInterpreter(rsrcmgr, device)
-                    for page in PDFPage.create_pages(doc):
-                        interpreter.process_page(page)
+                    text = ""
+                    for page in pdf_file:
+                        text += page.get_text("text")
 
+                    txt_output_file.write(text)
+            
                 with open(os.path.join(xml_directory, f"xml{suffix}.xml"), "w", encoding="utf-8") as xml_output_file:
-                    parser = PDFParser(pdf_file)
-                    doc = PDFDocument(parser)
-                    rsrcmgr = PDFResourceManager()
-                    codec = 'utf-8'
-                    device = XMLConverter(rsrcmgr, xml_output_file, laparams=LAParams())
-                    interpreter = PDFPageInterpreter(rsrcmgr, device)
-                    for page in PDFPage.create_pages(doc):
-                        interpreter.process_page(page)
+                    xml = ""
+                    for page in pdf_file:
+                        xml += page.get_text("xml")
+
+                    xml_output_file.write(xml)
+
+
+
     
 def convert():
     prepDir([txt_directory, xml_directory])
